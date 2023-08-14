a325eb0e8d6cfb9cb5ae4ac6710b5b9461d43e64
Mustafa Dayanır <mstfdynr@gmail.com>
2023-08-14 03:24:27 +0300

    fix gradle version

--- src/main/java/com/example/demo/WelcomeController.java
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -1,12 +1,5 @@
 package com.example.demo;
 
-import com.ydsdil.opennlp.model.RootWord;
-import io.reactivex.schedulers.Schedulers;
-import opennlp.tools.postag.POSModel;
-import opennlp.tools.postag.POSTaggerME;
-import opennlp.tools.tokenize.Tokenizer;
-import opennlp.tools.tokenize.TokenizerME;
-import opennlp.tools.tokenize.TokenizerModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +9,7 @@ import java.io.InputStream;
 
 @RestController
 public class WelcomeController {
-    
+
     @GetMapping({"/", "/home"})
     public String home() {
         return "Merhaba!";
